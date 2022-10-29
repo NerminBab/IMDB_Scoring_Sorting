fe0f85ad205fb9db2a2eaf48632210d304d20fc0
Nermin Babalik <nrmnbabalik@gmail.com>
2022-10-29 17:48:32 +0300

    dataset okutuldu

--- Sorting_Products.py
+++ Sorting_Products.py
@@ -5,7 +5,7 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.float_format', lambda x: '%.5' % x)
 
-df = pd.read_csv("", low_memory=False)
+df = pd.read_csv("movies_metadata.csv", low_memory=False)
 
 df = df[["title", "vote_average", "vote_count"]]
 
